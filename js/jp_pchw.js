5a8d318c57cdff606da16d0cdf247a68b3f2b7fa
denghuo369 <ybiancheng@163.cm>
2025-08-10 13:27:43 +0800

    直播

--- js/jp_pchw.js
+++ js/jp_pchw.js
@@ -2,7 +2,7 @@ var rule = {
     title: '荐片加速',
     host: 'https://pchwjiasu.abkc4.com/',
     homeUrl: '/', // 首页
-    url: '/?s=vod-type-id-fyclass-order-hits_week-pg{fypage}.html', // 分类列表接口
+    url: '/?s=vod-type-id-fyclass-order-hits_week.html', // 分类列表接口 - 移除分页参数，因为网站不支持URL分页
     class_name: '电影&电视剧&动漫&综艺&纪录片',
     class_url: '1&2&3&4&22',
     detailUrl: '/?s=vod-detail-id-fyid.html', // 详情接口
@@ -32,7 +32,7 @@ var rule = {
     },
     timeout: 5000,
     limit: 8,
-    pagecount: 10, // 分页数量
+    // 移除pagecount，因为网站不支持URL分页
     play_parse: true,
     play_json: [{
         re: '*',
@@ -45,12 +45,12 @@ var rule = {
     推荐: '.list-main li;a&&title;img&&data-original;.mark&&Text;a&&onclick',
     一级: '.list-main li;a&&title;img&&data-original;.mark&&Text;a&&onclick',
     二级: {
-        title: '.m-title h1&&Text',
+        title: '.m-title&&Text', // 修复：直接使用.m-title，不需要h1
         img: '.m-i-pic img&&data-original',
-        desc: "js:let html=request(input);let desc='';let title=html.match(/<h1[^>]*>([^<]+)<\/h1>/);if(title)desc+=title[1]+'\\n';let actor=html.match(/主演：<\/span><a[^>]*>([^<]+)<\/a>/);if(actor)desc+='主演：'+actor[1]+'\\n';let director=html.match(/导演：<\/span><a[^>]*>([^<]+)<\/a>/);if(director)desc+='导演：'+director[1]+'\\n';let type=html.match(/类型：<\/span><a[^>]*>([^<]+)<\/a>/);if(type)desc+='类型：'+type[1]+'\\n';let year=html.match(/上映：<\/span>([^<]+)/);if(year)desc+='上映：'+year[1]+'\\n';let area=html.match(/地区：<\/span>([^<]+)/);if(area)desc+='地区：'+area[1]+'\\n';let update=html.match(/更新：([^<]+)/);if(update)desc+='更新：'+update[1];setResult(desc);",
+        desc: "js:let html=request(input);let desc='';let title=html.match(/<div[^>]*class=\"[^\"]*m-title[^\"]*\"[^>]*>([^<]+)/);if(title)desc+=title[1].replace(/<[^>]*>/g,'')+'\\n';let actor=html.match(/主演：<\/span><a[^>]*>([^<]+)<\/a>/);if(actor)desc+='主演：'+actor[1]+'\\n';let director=html.match(/导演：<\/span><a[^>]*>([^<]+)<\/a>/);if(director)desc+='导演：'+director[1]+'\\n';let type=html.match(/类型：<\/span><a[^>]*>([^<]+)<\/a>/);if(type)desc+='类型：'+type[1]+'\\n';let year=html.match(/上映：<\/span>([^<]+)/);if(year)desc+='上映：'+year[1]+'\\n';let area=html.match(/地区：<\/span>([^<]+)/);if(area)desc+='地区：'+area[1]+'\\n';let update=html.match(/更新：([^<]+)/);if(update)desc+='更新：'+update[1];setResult(desc);",
         content: '.md-info&&Text',
-        tabs: '',
-        lists: "js:let d=[];let html=request(input);console.log('Extracting play URLs...');let playUrls=[];let playNames=[];let m=html.match(/g_PlayUrl='([\\s\\S]*?)';/);if(m && m[1] && m[1].length>0){console.log('Found g_PlayUrl, length:', m[1].length);let arr=m[1].split('#');for(let i=0;i<arr.length;i++){let kv=arr[i].split('$');if(kv.length==2){playNames.push(kv[0]);playUrls.push(kv[1]);}}}if(playUrls.length==0){console.log('g_PlayUrl empty, trying script content...');let scriptMatch=html.match(/<script>g_PlayUrl='([\\s\\S]*?)';<\\/script>/);if(scriptMatch && scriptMatch[1] && scriptMatch[1].length>0){console.log('Found script content, length:', scriptMatch[1].length);let arr=scriptMatch[1].split('#');for(let i=0;i<arr.length;i++){let kv=arr[i].split('$');if(kv.length==2){playNames.push(kv[0]);playUrls.push(kv[1]);}}}}if(playUrls.length==0){console.log('Still no URLs, trying onclick method...');let onclickMatches=html.match(/onclick=\"TT\\.History\\.Insert\\([^)]+\\)/g);if(onclickMatches){for(let i=0;i<onclickMatches.length;i++){let match=onclickMatches[i];let nameMatch=match.match(/第([^']+)集/);let urlMatch=match.match(/ftp:\\/\\/[^']+/);if(nameMatch && urlMatch){playNames.push('第'+nameMatch[1]+'集');playUrls.push(urlMatch[0]);}}}}if(playUrls.length==0){console.log('No play URLs found, checking for movie play buttons...');let playButtons=html.match(/<a[^>]*class=\"[^\"]*play[^\"]*\"[^>]*>([^<]+)<\/a>/g);if(playButtons){for(let i=0;i<playButtons.length;i++){let button=playButtons[i];let nameMatch=button.match(/>([^<]+)</);if(nameMatch){playNames.push(nameMatch[1]);playUrls.push('movie_play_'+i);}}}}console.log('Total play options found:', playUrls.length);for(let i=0;i<playUrls.length;i++){d.push([playNames[i]||'播放'+i, playUrls[i]]);}setResult([d]);"
+        tabs: '道长在线', // 恢复tabs设置
+        lists: "js:let d=[];let html=request(input);console.log('Extracting play URLs...');let playUrls=[];let playNames=[];let m=html.match(/g_PlayUrl='([\\s\\S]*?)';/);if(m && m[1] && m[1].length>0){console.log('Found g_PlayUrl, length:', m[1].length);let arr=m[1].split('#');for(let i=0;i<arr.length;i++){let kv=arr[i].split('$');if(kv.length==2){playNames.push(kv[0]);playUrls.push(kv[1]);}}}if(playUrls.length==0){console.log('g_PlayUrl empty, trying script content...');let scriptMatch=html.match(/<script>g_PlayUrl='([\\s\\S]*?)';<\\/script>/);if(scriptMatch && scriptMatch[1] && scriptMatch[1].length>0){console.log('Found script content, length:', scriptMatch[1].length);let arr=scriptMatch[1].split('#');for(let i=0;i<arr.length;i++){let kv=arr[i].split('$');if(kv.length==2){playNames.push(kv[0]);playUrls.push(kv[1]);}}}}if(playUrls.length==0){console.log('Still no URLs, trying onclick method...');let onclickMatches=html.match(/onclick=\"TT\\.History\\.Insert\\([^)]+\\)/g);if(onclickMatches){for(let i=0;i<onclickMatches.length;i++){let match=onclickMatches[i];let nameMatch=match.match(/第([^']+)集/);let urlMatch=match.match(/ftp:\\/\\/[^']+/);if(nameMatch && urlMatch){playNames.push('第'+nameMatch[1]+'集');playUrls.push(urlMatch[0]);}}}if(playUrls.length==0){console.log('No play URLs found, checking for movie play buttons...');let playButtons=html.match(/<a[^>]*class=\"[^\"]*play[^\"]*\"[^>]*>([^<]+)<\/a>/g);if(playButtons){for(let i=0;i<playButtons.length;i++){let button=playButtons[i];let nameMatch=button.match(/>([^<]+)</);if(nameMatch){playNames.push(nameMatch[1]);playUrls.push('movie_play_'+i);}}}}console.log('Total play options found:', playUrls.length);for(let i=0;i<playUrls.length;i++){d.push([playNames[i]||'播放'+i, playUrls[i]]);}setResult([d]);"
     },
     搜索: '.search-main li.s-m-item;.s-t-title h4 a&&Text;a.pic img&&data-original;a.pic span.mark&&Text;a.pic&&onclick'
 }; 
